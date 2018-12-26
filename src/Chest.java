7a198615e6d491f89bd7cd0805feb4ede469853c
Cynthia He <8933832+cynthhe@users.noreply.github.com>
2018-12-26 15:02:49 -0500

    Update Chest.java

--- src/Chest.java
+++ src/Chest.java
@@ -1,27 +1,3 @@
-/*
- * Chest.java
- * Author: Cynthia He, Laurenz Holcik, Evan Howell
- * Submission Date: November 08, 2018
- *
- * Purpose: This program stores information on whether the chest is locked or not, what's
- * inside the chest, and which key the chest was locked with.
- *
- * Statement of Academic Honesty:
- *
- * The following code represents my own work. I have neither
- * received nor given inappropriate assistance. I have not copied
- * or modified code from any source other than the course webpage
- * or the course textbook. I recognize that any unauthorized
- * assistance or plagiarism will be handled in accordance with
- * the University of Georgia's Academic Honesty Policy and the
- * policies of this course. I recognize that my work is based
- * on an assignment created by the Department of Computer
- * Science at the University of Georgia. Any publishing
- * or posting of source code for this project is strictly
- * prohibited unless you have written consent from the Department
- * of Computer Science at the University of Georgia.
- */
-
 public class Chest {
 
     private String contents;
